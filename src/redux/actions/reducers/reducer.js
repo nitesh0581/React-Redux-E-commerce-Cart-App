38458b1f992528cdd00e5a1a9ed5bb453268de42
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-09-16 14:34:54 +0530

    Fixed Delete button

--- src/redux/actions/reducers/reducer.js
+++ src/redux/actions/reducers/reducer.js
@@ -1,30 +1,30 @@
-
 const INIT_STATE = {
-    carts: [],
+	carts: [],
 }
 
 const cartReducer = (state = INIT_STATE, action) => {
 
-    switch (action.type) {
-        case "ADD_CART": {
-            return {
-                ...state,
-                carts: [...state.carts, action.payload]
-            }
-        }
-        case "RMV_CART":{
+	switch (action.type) {
+		case "ADD_CART": {
+			return {
+				...state,
+				carts: [...state.carts, action.payload]
+			}
+		}
+		case "RMV_CART": {
 
-            const data=state.carts.filter((e)=>e.id===action.payload);
-            return{
-                ...state,
-                carts:data,
-            }
-        }
+			const data = state.carts.filter((e) => e.id !== action.payload);
+			console.log({data, payload: action.payload})
+			return {
+				...state,
+				carts: data,
+			}
+		}
 
-        default:
-            return state
-    }
-        
+		default:
+			return state
+	}
+
 }
 
 export default cartReducer;
