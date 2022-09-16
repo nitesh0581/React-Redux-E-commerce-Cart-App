bb8390e7be1b499f7cfa8b3c5ba6785cb0526100
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-09-16 13:56:15 +0530

    fixed

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,61 +5,61 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import "./Style.css";
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import {Button, CardActionArea, CardActions} from '@mui/material';
 import {ADD} from "../redux/actions/action";
-import { useDispatch } from "react-redux";
+import {useDispatch} from "react-redux";
 
 
 function Cards() {
-    // const[data,setData]=useState(Cardsdata);
+	// const[data,setData]=useState(Cardsdata);
 
-    const dispatch=useDispatch();
+	const dispatch = useDispatch();
 
-    const sendData=(e)=>{
-        dispatch(ADD(e))
-    }
+	const sendData = (e) => {
+		dispatch(ADD(e))
+	}
 
-    return (
-        <div className='container mt-3'>
-            <h2 className='text-center'>Add to Cart Projects</h2>
+	return (
+		<div className='container mt-3'>
+			<h2 className='text-center'>Add to Cart Projects</h2>
 
-            <div className="row d-flex justify-content-center align-items-center">
-                {
-                    Cardsdata.map((element,id)=>{
-                        return(
-                            <>
-                    <Card sx={{ width:"20vw",height:"200", border:"none"}} className="mx-4 mt-4 card_style">
-                    <CardActionArea>
-                        <CardMedia className="mt-3"
-                            component="img"
-                            height="200"
-                            image={element.imgdata}
-                            alt="green iguana"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div">
-                               {element.rname}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                               Price: ₹{element.price}
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                    <CardActions style={{justifyContent: 'center'}} >
-                    
-                        <Button size="small" variant="contained" onClick={()=>sendData(element)}>
-                            Add to Cart
-                        </Button>
-                    </CardActions>
-                </Card>
-                            </>
-                        )
-                    })
-                }
+			<div className="row d-flex justify-content-center align-items-center">
+				{
+					Cardsdata.map((element, id) => {
+						return (
+							<React.Fragment key={Math.random()}>
+								<Card sx={{width: "20vw", height: "200", border: "none"}}
+								      className="mx-4 mt-4 card_style">
+									<CardActionArea>
+										<CardMedia className="mt-3"
+										           component="img"
+										           height="200"
+										           image={element.imgdata}
+										           alt="green iguana"
+										/>
+										<CardContent>
+											<Typography gutterBottom variant="h5" component="div">
+												{element.rname}
+											</Typography>
+											<Typography variant="body2" color="text.secondary">
+												Price: ₹{element.price}
+											</Typography>
+										</CardContent>
+									</CardActionArea>
+									<CardActions style={{justifyContent: 'center'}}>
 
-            </div>
-        </div>
-    )
+										<Button size="small" variant="contained" onClick={() => sendData(element)}>
+											Add to Cart
+										</Button>
+									</CardActions>
+								</Card>
+							</React.Fragment>
+						)
+					})
+				}
+			</div>
+		</div>
+	)
 };
 
 export default Cards;
--- src/components/cartDetails.js
+++ src/components/cartDetails.js
@@ -1,89 +1,95 @@
 import React from "react";
 import Table from '@mui/material/Table';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { useNavigate } from "react-router-dom";
-import { useSelector,useDispatch } from "react-redux";
+import {useNavigate} from "react-router-dom";
+import {useSelector, useDispatch} from "react-redux";
 import {DLT} from "../redux/actions/action";
 
 function CardDetails() {
 
-    // getting the id we are passing of particular product
-    // const { id } = useParams();
+	// getting the id we are passing of particular product
+	// const { id } = useParams();
 
-    const history = useNavigate();
+	const history = useNavigate();
 
-    const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-    const cartItems = useSelector((state) => state.rootReducer.cartReducer.carts);
-    // console.log(getData);
+	const cartItems = useSelector((state) => state.rootReducer.cartReducer.carts);
+	// console.log(getData);
 
-    // const compare = () => {
-    //     let compareData = getData.filter((e) => {
-    //         return e.id === id
-    //     });
-    //     setData(compareData);
-    // }
+	// const compare = () => {
+	//     let compareData = getData.filter((e) => {
+	//         return e.id === id
+	//     });
+	//     setData(compareData);
+	// }
 
-    //useEffect will call this function whenever id of selected product changes
-    // useEffect(() => {
-    //     let compareData = getData.filter((e) => {
-    //         return e.id === id
-    //     });
-    //     setData(compareData);
-    // }, [id])
+	//useEffect will call this function whenever id of selected product changes
+	// useEffect(() => {
+	//     let compareData = getData.filter((e) => {
+	//         return e.id === id
+	//     });
+	//     setData(compareData);
+	// }, [id])
 
 
-    const dlt = (id)=>{
-        dispatch(DLT(id));
-        history("/");
-    }
+	const dlt = (id) => {
+		dispatch(DLT(id));
+		history("/");
+	}
 
-    return (
-        <div className="container mt-2">
-            <h2 className="text-center">My Cart Page</h2>
+	return (
+		<div className="container mt-2">
+			<h2 className="text-center">My Cart Page</h2>
 
-            <section className="container mt-3">
-                <div className="itemsdetails">
-                    {
-                        cartItems.map((ele) => {
-                            return (
-                                <>
-                                    <div className="items_img">
-                                        <img src={ele.imgdata} alt="" style={{maxHeight:"300px"}}/>
-                                    </div>
+			<section className="container mt-3">
+				<div className="itemsdetails">
+					{
+						cartItems.map((ele) => {
+							return (
+								<React.Fragment key={Math.random()}>
+									<div className="items_img">
+										<img src={ele.imgdata} alt="" style={{maxHeight: "300px"}}/>
+									</div>
 
-                                    <div className="details">
-                                        <Table>
-                                            <tr>
-                                                <td>
-                                                    <p><strong>Resturant:</strong> {ele.rname}</p>
-                                                    <br/>
-                                                    <p><strong>Price:</strong> ₹ {ele.price}</p>
-                                                    <br/>
-                                                    <p><strong>Dishes:</strong> {ele.address}</p>
-                                                    <br/>
-                                                    <p><strong>Total:</strong> ₹ 300</p>
-                                                </td>
-                                                <td>
-                                                    <p ><strong>Rating: </strong><span style={{ backgroundColor: "green", padding: "2px 2px", borderRadius: "5px", color: "white" }}> {ele.rating}★</span></p>
-                                                    <br/>
-                                                    <p><strong>Order Review: </strong> {ele.somedata}</p>
-                                                    <br/>
-                                                    <p><strong>Remove: </strong><DeleteIcon style={{ cursor: "pointer", color: "red", fontSize: "25" }} onClick={dlt(ele.id)}/></p>
-                                                    <br/>
-                                                </td>
-                                            </tr>
-                                        </Table>
-                                    </div>
-                                </>
-                            )
-                        })
-                    }
+									<div className="details">
+										<Table>
+											<tr>
+												<td>
+													<p><strong>Resturant:</strong> {ele.rname}</p>
+													<br/>
+													<p><strong>Price:</strong> ₹ {ele.price}</p>
+													<br/>
+													<p><strong>Dishes:</strong> {ele.address}</p>
+													<br/>
+													<p><strong>Total:</strong> ₹ 300</p>
+												</td>
+												<td>
+													<p><strong>Rating: </strong><span style={{
+														backgroundColor: "green",
+														padding: "2px 2px",
+														borderRadius: "5px",
+														color: "white"
+													}}> {ele.rating}★</span></p>
+													<br/>
+													<p><strong>Order Review: </strong> {ele.somedata}</p>
+													<br/>
+													<p><strong>Remove: </strong><DeleteIcon
+														style={{cursor: "pointer", color: "red", fontSize: "25"}} onClick={() => dlt(ele.id)}/></p>
+													<br/>
+												</td>
+											</tr>
+										</Table>
+									</div>
+								</React.Fragment>
+							)
+						})
+					}
 
-                </div>
-            </section>
-        </div>
-    )
+				</div>
+			</section>
+		</div>
+	)
 };
 
 export default CardDetails;
