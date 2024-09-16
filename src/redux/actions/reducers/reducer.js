d871ec264fcbcb9fbec4f6ca9b3493dcca63fe4e
nitesh.sharma <nitesh.sharma@etelligens.in>
2024-09-17 02:02:13 +0530

    quantity add or minus feature added in cards as well

--- src/redux/actions/reducers/reducer.js
+++ src/redux/actions/reducers/reducer.js
@@ -1,74 +1,73 @@
 const INIT_STATE = {
-  carts: [],
-};
-
-const cartReducer = (state = INIT_STATE, action) => {
-  switch (action.type) {
-    case "ADD_CART": {
-      const itemIndex = state.carts.findIndex(
-        (item) => item.id === action.payload.id
-      );
-
-      // if qnty when add item
-      if (itemIndex >= 0) {
-        return {
-          ...state,
-          carts: state.carts?.map((item, index) =>
-            index === itemIndex ? { ...item, qnty: item.qnty + 1 } : item
-          ),
-        };
-      } else {
-        // new item added to cart
-        const newItem = { ...action.payload, qnty: 1 };
-        return {
-          ...state,
-          carts: [...state.carts, newItem],
-        };
-      }
-    }
-    case "RMV_CART": {
-      const filteredData = state.carts.filter((e) => e.id !== action.payload);
-      return {
-        ...state,
-        carts: filteredData,
-      };
-    }
-
-    case "RMV_ONE": {
-      const itemIndexRmv = state.carts.findIndex(
-        (item) => item.id === action.payload.id
-      );
-      if (itemIndexRmv >= 0) {
-        const itemQnty = state.carts[itemIndexRmv];
-        // if qnty is more than 1 then remove one onclick
-        if (itemQnty > 1) {
+    carts: [],
+  };
+  
+  const cartReducer = (state = INIT_STATE, action) => {
+    switch (action.type) {
+      case "ADD_CART": {
+        const itemIndex = state.carts.findIndex((item) => item.id === action.payload.id);
+  
+        if (itemIndex >= 0) {
+          // Item already exists, so update quantity immutably
           return {
             ...state,
-            carts: state?.carts?.map((item) =>
-              item?.id === itemIndexRmv
-                ? { ...item, qnty: item?.qnty - 1 }
+            carts: state.carts.map((item, index) =>
+              index === itemIndex
+                ? { ...item, qnty: item.qnty + 1 } // Increase quantity immutably
                 : item
             ),
           };
-        }
-        // but if qnty is already 1 then on on click dlt that item
-        else if (state.carts[itemIndexRmv].qnty === 1) {
-          const filteredData = state.carts.filter(
-            (el) => el.id !== action.payload
-          );
-
+        } else {
+          // New item, add to the cart
+          const newItem = { ...action.payload, qnty: 1 };
           return {
             ...state,
-            carts: filteredData,
+            carts: [...state.carts, newItem],
           };
         }
       }
-      return state;
+  
+      case "RMV_CART": {
+        // Remove item from the cart
+        const filteredCarts = state.carts.filter((item) => item.id !== action.payload);
+        return {
+          ...state,
+          carts: filteredCarts,
+        };
+      }
+  
+      case "RMV_ONE": {
+        const itemIndex = state.carts.findIndex((item) => item.id === action.payload.id);
+  
+        if (itemIndex >= 0) {
+          const item = state.carts[itemIndex];
+  
+          if (item.qnty > 1) {
+            // Decrease quantity immutably
+            return {
+              ...state,
+              carts: state.carts.map((cartItem, index) =>
+                index === itemIndex
+                  ? { ...cartItem, qnty: cartItem.qnty - 1 } // Decrease quantity immutably
+                  : cartItem
+              ),
+            };
+          } else if (item.qnty === 1) {
+            // Remove item if quantity is 1
+            const filteredCarts = state.carts.filter((cartItem) => cartItem.id !== action.payload.id);
+            return {
+              ...state,
+              carts: filteredCarts,
+            };
+          }
+        }
+        return state;
+      }
+  
+      default:
+        return state;
     }
-    // eslint-disable-next-line
-    default:
-      return state;
-  }
-};
-
-export default cartReducer;
+  };
+  
+  export default cartReducer;
+  
