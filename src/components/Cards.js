2a07d454d9aaf1cfc83047c75e97c25d36cc34a0
nitesh.sharma <nitesh.sharma@etelligens.in>
2024-09-17 01:59:21 +0530

    changes done

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,63 +1,117 @@
-import React from "react";
+import React, { useState } from "react";
 import Cardsdata from "./CardData";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 import "./Style.css";
-import { Button, CardActionArea, CardActions } from '@mui/material';
-import { ADD } from "../redux/actions/action";
-import { useDispatch} from "react-redux";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+import { ADD, DLT, REMOVE } from "../redux/actions/action";
+import { useDispatch, useSelector } from "react-redux";
 
 function Cards() {
+  const dispatch = useDispatch();
+  const [showMinus, setShowMinus] = useState(false);
+  const getData = useSelector((state) => state.rootReducer.cartReducer.carts);
+  console.log(getData, "getdata");
+  const [itemQuantity, setItemQuantity] = useState([]);
+  const sendData = (e) => {
+    dispatch(ADD(e));
 
-	const dispatch = useDispatch();
-	
-	const sendData = (e) => {
-			dispatch(ADD(e));
-	}
+    const quantity = getData?.map((item) => item?.id);
+    setItemQuantity(quantity);
+  };
+  console.log(itemQuantity, "xcvb");
+  const dlt = (id) => {
+    dispatch(DLT(id));
+  };
+  const remove = (e) => {
+    dispatch(REMOVE(e));
+  };
+  const send = (e) => {
+    dispatch(ADD(e));
+  };
 
-	return (
-		<div className='container mt-3'>
-			<h2 className='text-center'>Add to Cart App</h2>
+  return (
+    <div className="container mt-3">
+      <h2 className="text-center">Add to Cart App</h2>
 
-			<div className="row d-flex justify-content-center align-items-center product">
-				{
-					Cardsdata.map((element, id) => {
-						return (
-							<React.Fragment key={Math.random()}>
-								<Card sx={{ width: "20vw", height: "200", border: "none" }}
-									className="mx-4 mt-4 card_style">
-									<CardActionArea>
-										<CardMedia className="mt-3"
-											component="img"
-											height="200"
-											image={element.imgdata}
-											alt="green iguana"
-										/>
-										<CardContent>
-											<Typography gutterBottom variant="h5" component="div">
-												{element.rname}
-											</Typography>
-											<Typography variant="body2" color="text.secondary">
-												Price: ₹{element.price}
-											</Typography>
-										</CardContent>
-									</CardActionArea>
-									<CardActions style={{ justifyContent: 'center' }}>
+      <div className="row d-flex justify-content-center align-items-center product">
+        {Cardsdata.map((element, id) => {
+          return (
+            <React.Fragment key={Math.random()}>
+              <Card
+                sx={{ width: "20vw", height: "200", border: "none" }}
+                className="mx-4 mt-4 card_style"
+              >
+                <CardActionArea>
+                  <CardMedia
+                    className="mt-3"
+                    component="img"
+                    height="200"
+                    image={element.imgdata}
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {element.rname}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Price: ₹{element.price}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions className="d-flex justify-content-center">
+                  {getData.find((el) => el?.id === element?.id)?.qnty >= 1 ? (
+                    <div
+                      className="d-flex justify-content-between align-items-center"
+                      style={{
+                        width: 100,
+                        cursor: "pointer",
+                        background: "#ddd",
+                        color: "#111",
+                        padding: "2px",
+                        borderRadius: 9,
+                      }}
+                    >
+                      <span
+                        onClick={
+                          getData.find((el) => el?.id === element?.id)?.qnty > 1
+                            ? () => remove(element)
+                            : () => dlt(element.id)
+                        }
+                      >
+                        <RemoveIcon />
+                      </span>
 
-										<Button size="small" variant="contained" onClick={() => sendData(element)}>
-											Add to Cart
-										</Button>
-									</CardActions>
-								</Card>
-							</React.Fragment>
-						)
-					})
-				}
-			</div>
-		</div>
-	)
-};
+                      <span style={{ fontSize: 22 }}>
+                        {getData.some((el) => el?.id === element?.id)
+                          ? getData.find((el) => el?.id === element?.id)?.qnty
+                          : 0}
+                      </span>
+                      <span onClick={() => send(element)}>
+                        <AddIcon />
+                      </span>
+                    </div>
+                  ) : (
+                    <Button
+                      size="small"
+                      variant="contained"
+                      onClick={() => sendData(element)}
+                    >
+                      Add to Cart
+                    </Button>
+                  )}
+                </CardActions>
+              </Card>
+            </React.Fragment>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
 
 export default Cards;
--- src/redux/actions/reducers/reducer.js
+++ src/redux/actions/reducers/reducer.js
@@ -1,76 +1,74 @@
+const INIT_STATE = {
+  carts: [],
+};
 
-    const INIT_STATE = {
-        carts: [],
+const cartReducer = (state = INIT_STATE, action) => {
+  switch (action.type) {
+    case "ADD_CART": {
+      const itemIndex = state.carts.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      // if qnty when add item
+      if (itemIndex >= 0) {
+        return {
+          ...state,
+          carts: state.carts?.map((item, index) =>
+            index === itemIndex ? { ...item, qnty: item.qnty + 1 } : item
+          ),
+        };
+      } else {
+        // new item added to cart
+        const newItem = { ...action.payload, qnty: 1 };
+        return {
+          ...state,
+          carts: [...state.carts, newItem],
+        };
+      }
+    }
+    case "RMV_CART": {
+      const filteredData = state.carts.filter((e) => e.id !== action.payload);
+      return {
+        ...state,
+        carts: filteredData,
+      };
     }
 
-    const cartReducer = (state = INIT_STATE, action) => {
-
-   
-        switch (action.type) {
-            case "ADD_CART": {
-
-                const itemIndex=state.carts.findIndex((item)=>item.id=== action.payload.id);
-
-                // if qnty when add item
-                if(itemIndex >=0){
-                    state.carts[itemIndex].qnty+=1;
-                    return {
-                        ...state,
-                        carts: [...state.carts]
-                    }
-                }else{
-                    // if qnty is not repeating then we can directly make qnty 1
-                    const temp={...action.payload, qnty:1}
-                    return{
-                        ...state,
-                        carts:[...state.carts, temp]
-                }
-                } 
-                                                                                                                       
-            }
-            case "RMV_CART":{
-
-                const data=state.carts.filter((e)=>e.id!==action.payload);
-                return{
-                    ...state,
-                    carts:data,
-                }
-            }
-
-            case "RMV_ONE":{
-
-                const itemIndex_Dec=state.carts.findIndex((item)=>item.id===action.payload.id);
-                
-                // if qnty is more than 1 then remove one onclick
-                if(state.carts[itemIndex_Dec].qnty >= 1){                                                                                                   
-                    const dltiteams = state.carts[itemIndex_Dec].qnty -= 1
-                    console.log([...state.carts, dltiteams]);
-                   
-                    
-                    return {
-                        ...state,
-                        carts:[...state.carts],
-                        
-                    }
-                    
-                }
-                // but if qnty is already 1 then on on click dlt that item
-                else if(state.carts[itemIndex_Dec].qnty === 1 ){
-                    const data = state.carts.filter((el)=>el.id !== action.payload);
-                    
-                   
-                    return {       
-                        ...state,
-                        carts:data
-                    }
-                   
-                }
-            }
-        // eslint-disable-next-line
-            default:
-                return state
+    case "RMV_ONE": {
+      const itemIndexRmv = state.carts.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (itemIndexRmv >= 0) {
+        const itemQnty = state.carts[itemIndexRmv];
+        // if qnty is more than 1 then remove one onclick
+        if (itemQnty > 1) {
+          return {
+            ...state,
+            carts: state?.carts?.map((item) =>
+              item?.id === itemIndexRmv
+                ? { ...item, qnty: item?.qnty - 1 }
+                : item
+            ),
+          };
         }
-            
-    }
+        // but if qnty is already 1 then on on click dlt that item
+        else if (state.carts[itemIndexRmv].qnty === 1) {
+          const filteredData = state.carts.filter(
+            (el) => el.id !== action.payload
+          );
 
-    export default cartReducer;
+          return {
+            ...state,
+            carts: filteredData,
+          };
+        }
+      }
+      return state;
+    }
+    // eslint-disable-next-line
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
